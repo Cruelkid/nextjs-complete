b525057f20f64181655f79a0a16b9bf14312320e
Cruelkid <sonofammit@gmail.com>
2021-10-25 23:17:58 +0300

    added meeting details page

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,23 +1,34 @@
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 function MeetupItem(props) {
-  return (
-    <li className={classes.item}>
-      <Card>
-        <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
-        </div>
-        <div className={classes.content}>
-          <h3>{props.title}</h3>
-          <address>{props.address}</address>
-        </div>
-        <div className={classes.actions}>
-          <button>Show Details</button>
-        </div>
-      </Card>
-    </li>
-  );
+    const router = useRouter();
+
+    function redirectHandler() {
+        router.push('/' + props.id);
+    }
+
+    return (
+        <li className={classes.item}>
+            <Card>
+                <div className={classes.image}>
+                    <img src={props.image} alt={props.title} />
+                </div>
+                <div className={classes.content}>
+                    <h3>{props.title}</h3>
+                    <address>{props.address}</address>
+                </div>
+                <div className={classes.actions}>
+                    <Link href={'/' + props.id}>
+                        <a className="btn btn-outline-primary">Show Details</a>
+                    </Link>
+                    {/* <button onClick={redirectHandler}>Show Details</button> */}
+                </div>
+            </Card>
+        </li>
+    );
 }
 
 export default MeetupItem;
---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+    return (
+        <MeetupDetail
+            image="https://upload.wikimedia.org/wikipedia/commons/1/1a/Old_city_hall_of_Leipzig_%2820%29.jpg"
+            title="First meetup"
+            address="Markt 1, 04109 Leipzig, Germany"
+            description="This is a first meetup"
+        />
+    );
+}
+
+export default MeetupDetails;
